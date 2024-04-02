92caaf92d5a2a81ce3b817974c272771f7e8d9b7
Gabriele <dardino@users.noreply.github.com>
2024-04-02 14:13:13 +0200

    Adds DrillManagement and LoadBalancer

---
+++ SpaceEngeneersQOL/DrillManagement/Trivella.cs
@@ -0,0 +1,184 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace SpaceEngeneersQOL.DrillManagement
+{
+    partial class Trivella : MyGridProgram
+    {
+        private readonly float angleThreshold = 0.1f;
+        private readonly float fillThreshold = 0.9f;
+        private float nextTargetAngle = 0.0f;
+
+        public Trivella()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+        }
+
+        private float GetReachableCargoFillRatio(VRage.Game.ModAPI.Ingame.IMyInventory referenceInventory)
+        {
+
+            List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock> { };
+            GridTerminalSystem.GetBlocks(blocks);
+
+            var blocksInTheGrid = blocks
+                .Where((block) =>
+                    block.IsSameConstructAs(Me) &&
+                    block.GetInventory() != null &&
+                    referenceInventory.IsConnectedTo(block.GetInventory())
+                );
+
+            var currentTotalVolume = 0.0f;
+            var maxTotalVolume = 0.0f;
+
+            foreach (var block in blocksInTheGrid)
+            {
+                var inventory = block.GetInventory();
+                currentTotalVolume += ((float)inventory.CurrentVolume);
+                maxTotalVolume += ((float)inventory.MaxVolume);
+            }
+
+            var totalFillRatio = currentTotalVolume / maxTotalVolume;
+
+            return totalFillRatio;
+        }
+
+        private bool CheckExistence(Object entity, IMyTextPanel output, String entityName)
+        {
+            if (output == null)
+            {
+                Echo("no output for debugging");
+                return false;
+            }
+
+            if (entity == null)
+            {
+                output.WriteText($"[color='#FF0000'] entity not found: {entityName}");
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool HasCompletedFullTurn(IMyMotorStator rotor)
+        {
+            return rotor.Angle < nextTargetAngle + angleThreshold && rotor.Angle >= nextTargetAngle - angleThreshold;
+        }
+
+        private bool TurnOn(IMyTerminalBlock entity)
+        {
+            if (entity != null)
+            {
+                entity.ApplyAction("OnOff_On");
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool TurnOff(IMyTerminalBlock entity)
+        {
+            if (entity != null)
+            {
+                entity.ApplyAction("OnOff_Off");
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private void IncreaseMaxDistanceAndMove(IMyPistonBase piston)
+        {
+            piston.ApplyAction("IncreaseUpperLimit");
+            piston.ApplyAction("Extend");
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            var pistons = GridTerminalSystem.GetBlockGroupWithName("Trivella.Pistons");
+            var drills = GridTerminalSystem.GetBlockGroupWithName("Trivella.Drills");
+            var rotor = GridTerminalSystem.GetBlockWithName("Trivella.Rotor") as IMyMotorStator;
+            var light = GridTerminalSystem.GetBlockWithName("Trivella.DebugLight");
+            var lcd = GridTerminalSystem.GetBlockWithName("Trivella.TextPanel") as IMyTextPanel;
+
+            if (!CheckExistence(pistons, lcd, "Pistons Group")) return;
+            if (!CheckExistence(drills, lcd, "Drills Group")) return;
+            if (!CheckExistence(rotor, lcd, "Rotor")) return;
+            if (!CheckExistence(light, lcd, "Light")) return;
+
+            var pistonsBlockList = new List<IMyTerminalBlock> { };
+            var drillsBlockList = new List<IMyTerminalBlock> { };
+
+            pistons.GetBlocks(pistonsBlockList);
+            drills.GetBlocks(drillsBlockList);
+
+            lcd.SetValue("FontColor", Color.Green);
+
+            // select current piston (ordered by the number in the name)
+            var currentPiston = pistonsBlockList
+                .Where((pistonBlock) => (pistonBlock as IMyPistonBase).MaxLimit < (pistonBlock as IMyPistonBase).HighestPosition)
+                .OrderBy((pistonBlock) => float.Parse(pistonBlock.CustomName.Split(' ')[1]))
+                .ToList()
+                .First();
+            var fillRatio = GetReachableCargoFillRatio(drillsBlockList[0].GetInventory());
+
+            if (fillRatio > fillThreshold)
+            {
+                // storage is full, shut everything down
+                drillsBlockList.ForEach((drillBlock) =>
+                {
+                    TurnOff(drillBlock);
+                    TurnOff(rotor);
+                });
+            }
+            else
+            {
+                drillsBlockList.ForEach((drillBlock) =>
+                {
+                    TurnOn(drillBlock);
+                });
+                TurnOn(rotor);
+            }
+
+            if (HasCompletedFullTurn(rotor))
+            {
+                if (nextTargetAngle == 0) nextTargetAngle = 3.14f;
+                else nextTargetAngle = 0;
+                TurnOn(light);
+                IncreaseMaxDistanceAndMove(currentPiston as IMyPistonBase);
+            }
+            else
+            {
+                TurnOff(light);
+            }
+
+            lcd.WriteText($"current angle: {rotor.Angle}\n");
+            lcd.WriteText($"station capacity at {(fillRatio * 100).ToString("0.00")}%\n\n", true);
+
+            pistonsBlockList.ForEach((pistonBlock) =>
+            {
+                lcd.WriteText($"{(pistonBlock.EntityId == currentPiston.EntityId ? "> " : "")}{pistonBlock.CustomName} at {(pistonBlock as IMyPistonBase).CurrentPosition} / {(pistonBlock as IMyPistonBase).MaxLimit} | {(pistonBlock as IMyPistonBase).HighestPosition}\n", true);
+            });
+        }
+
+    }
+}
---
+++ SpaceEngeneersQOL/LoadBalancer/LoadBalancer.cs
@@ -0,0 +1,111 @@
+﻿using Sandbox.Game;
+using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game.VisualScripting.Utils;
+using VRageMath;
+
+namespace SpaceEngeneersQOL.LoadBalancer
+{
+    partial class Program : MyGridProgram
+    {
+        #region -- CUT FROM HERE
+
+
+        public Program()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+        }
+
+        private float treshold = 0.06f;
+        private string ledPanelName = "LoadBalancer.LCD";
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            List<IMyAssembler> assemblers = new List<IMyAssembler>();
+            var lcd = GridTerminalSystem.GetBlockWithName(ledPanelName) as IMyTextPanel;
+            GridTerminalSystem.GetBlocksOfType(assemblers);
+            lcd.WritePublicTitle("Load Balancer");
+            var full = assemblers.Where(assembler => {
+                if (assembler.GetInventory() != null)
+                {
+                    float ratio = (float)assembler.GetInventory().CurrentVolume.ToIntSafe() / assembler.GetInventory().MaxVolume.ToIntSafe();
+                    return ratio >= 1 - treshold;
+                }
+                return false;
+            }).ToList();
+
+            lcd.WriteText($"Number of full assemblers: {full.Count} of {assemblers.Count}\n");
+            var withQueue = full.Where(inv => !inv.IsQueueEmpty).ToList();
+            lcd.WriteText($"Assemblers with queue: {withQueue.Count} of {full.Count}\n", true);
+            var inventoryBlocks = new List<IMyInventoryOwner>();
+            GridTerminalSystem.GetBlocksOfType(inventoryBlocks);
+            inventoryBlocks = inventoryBlocks.Where(block => !(block is IMyAssembler)
+                                      && block.UseConveyorSystem
+                                      && block.GetInventory(block.InventoryCount - 1) is IMyInventory
+                                      && !block.GetInventory(block.InventoryCount - 1).IsFull).ToList();
+            var myInventory = inventoryBlocks.Select(inv => inv.GetInventory(inv.InventoryCount - 1)).ToList();
+
+            if (withQueue.Count > 0)
+            {
+                lcd.WriteText($"Number of usable inventory: {inventoryBlocks.Count}\n", true);
+
+                if (inventoryBlocks.Count > 0 && withQueue.Count > 0) withQueue.ForEach(assembler => rearrange(assembler, lcd, myInventory));
+            }
+        }
+
+        private void rearrange(IMyAssembler assembler, IMyTextPanel lcd, List<IMyInventory> inventoryBlocks)
+        {
+            var inventory = assembler.GetInventory(0);
+            var items = new List<MyInventoryItem>();
+            inventory.GetItems(items);
+            lcd.WriteText($"{assembler.CustomName}:\t", true);
+            items = items.OrderBy(item => item.Amount.ToIntSafe()).ToList();
+            items.ForEach(item => {
+                lcd.WriteText($"{item.Type.SubtypeId}[{item.Amount.ToIntSafe() / 1000}k]\t", true);
+            });
+            lcd.WriteText("\n", true);
+
+            var itemToMove = items.Last();
+            var destinations = inventoryBlocks
+                .Where(destInv =>
+                {
+                    var can = inventory.CanTransferItemTo(destInv, itemToMove.Type);
+                    var itemsTypes = new List<MyItemType>();
+                    inventory.GetAcceptedItems(itemsTypes);
+                    can = can && itemsTypes.TrueForAll(typ => typ.TypeId == itemToMove.Type.TypeId);
+                    return can;
+                })
+                .OrderBy(destInv => destInv.CurrentVolume.ToIntSafe()).ToList();
+            if (destinations.Count < 1)
+            {
+                lcd.WriteText("! No destination found !\n", true);
+            }
+            foreach (var destination in destinations)
+            {
+                lcd.WriteText($"Try to move item {itemToMove.Type.SubtypeId} from {assembler.CustomName} to {destination.Owner} inventory... ", true);
+                var success = inventory.TransferItemTo(destination, itemToMove, itemToMove.Amount.ToIntSafe() / 2);
+                lcd.WriteText(success ? "done!" : "fail", true);
+                lcd.WriteText("\n", true);
+                if (success) break;
+            }
+
+        }
+        #endregion // -- TO HERE
+    }
+}
